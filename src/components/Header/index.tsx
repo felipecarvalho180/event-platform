2ea96af256a3a83a8b1f3edc248062818667137e
Felipe Carvalho <felipecarvalho180@gmail.com>
2022-06-21 20:48:43 -0300

    create header and sidebar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Brand } from "../Brand";
+
+export function Header() {
+  return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <Brand />
+    </header>
+  );
+}
